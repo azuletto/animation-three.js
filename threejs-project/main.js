c0a340a51144397673c4e9101bccf008e264c00a
André Oliveira <78551538+azuletto@users.noreply.github.com>
2024-07-23 18:23:10 -0300

    :wrench: applying textures
    having some erros with loading the textures and making de uv mapping

--- threejs-project/main.js
+++ threejs-project/main.js
@@ -6,6 +6,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+
 const cubes = [];
 const cubeSpeed = 0.05;
 
@@ -13,13 +14,15 @@ const cubeSpeed = 0.05;
 function createCube() {
   const height = Math.random() * (5 - 2) + 2; // Random height between 1 and 3
   const geometry = new THREE.BoxGeometry(1, height, 0.5);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const material = new THREE.MeshStandardMaterial({ 
+	map: buildingTexture
+   });
 
   const cube = new THREE.Mesh(geometry, material);
 
   // Set the initial x position of the new cube relative to the previous cube
   const previousCube = cubes[cubes.length - 1];
-  cube.position.x = previousCube ? previousCube.position.x - 5 : -window.innerWidth / 100;
+  cube.position.x = previousCube ? previousCube.position.x - 3 : -window.innerWidth / 100;
   
   cube.position.y = height/2 - 1.6; // Keep y position constant
   cube.position.z = 0; // Keep it on the same z-plane
@@ -28,13 +31,18 @@ function createCube() {
   scene.add(cube);
   console.log('Cube created at position:', cube.position.x, cube.position.y, cube.position.z, 'with height:', height);
 }
+const buildingTexture = new THREE.TextureLoader().load('./building-texture.jpg');
+const cityTexture = new THREE.TextureLoader().load('./city-back.jpeg');
+scene.background = cityTexture;
 
 // Initially create a few cubes
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 10; i++) {
   createCube();
 }
 
 camera.position.z = 5;
+
+
 
 function animate() {
   requestAnimationFrame(animate);
